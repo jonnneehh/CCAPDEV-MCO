a2d9d00addf17928cbe2b8f122ba4c89f3a26caa
Jon Llamado <jon_llamado@dlsu.edu.ph>
2022-05-19 20:03:15 +0800

    comment

---
+++ public/js/posts.js
@@ -0,0 +1,40 @@
+$(document).ready(()=>{
+    $("#addcomment").click(()=>{
+        var comment = $("#add-comment").val();
+        var user = "Anjit243"
+        var iconsrc = "images/u-profile.png";
+        
+        if(comment == ""){
+            return;
+        }
+        
+        $("#add-comment").val("");
+
+        addComment(comment, user, iconsrc);
+
+        //Will be a problem if we change the layout of our comment section
+        function addComment(comment, user, iconsrc){
+            $(".comments-section").append(
+                '<div class="first-tier">' +
+					'<div class=\"comment-card\">' +
+						'<img class=\"comment-icon\" src='+ iconsrc +'>' +
+						'<span class=\"user-handle">'+ user +'</h3>' +	
+					'</div>' +	
+					'<div class=\"comment-desc\">' +
+						'<span>'+ comment +'</span>' +
+					'</div>' +
+					'<div class=\"comment-action\">' +
+						'<i class=\"fa-solid fa-chevron-up fa-lg\"></i>' +
+						'<span class=\"comment-likes\">0</span>' +
+						'<i class=\"fa-solid fa-chevron-down fa-lg\"></i>' +
+						'<i class=\"fa-solid fa-comments fa-lg\"></i>' +
+					'</div>' +
+				'</div>'
+            );
+        }
+    })
+
+    $("#cancelcomment").click(()=>{
+        $("#add-comment").val("");
+    })
+})
