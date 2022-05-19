93614600fd90cf834e6d20071defab5a40b15473
Jon Llamado <jon_llamado@dlsu.edu.ph>
2022-05-20 00:55:34 +0800

    updoots and downdoots

--- public/js/posts.js
+++ public/js/posts.js
@@ -5,13 +5,12 @@ $(document).ready(()=>{
         $(this).remove();
     })
 
-    $("#addcomment").click(()=>{
+    $("#addcomment").click(function(){
         var comment = $("#add-comment").val();
         var user = "Anjit243"
         var iconsrc = "images/u-profile.png";
         
-        var postid = $('#addcomment').closest(".post-container").attr("id");
-        console.log(postid);
+        var postid = $(this).closest(".post-container").attr("id");
 
         if(comment == ""){
             return;
@@ -43,7 +42,67 @@ $(document).ready(()=>{
         }
     })
 
-    $("#cancelcomment").click(()=>{
+    $("#cancelcomment").click(function(){
         $("#add-comment").val("");
+    })
+
+    $(".upvote").click(function(){
+        var color = $(this).css("color");
+        var siblingcolor = $(this).siblings('.downvote').css("color");
+        const black = "rgb(38, 38, 38)";
+        const blue = "rgb(0, 0, 255)";
+        
+        if(siblingcolor != black){
+            $(this).siblings('.downvote').css("color", black);
+            addLike(this)
+        }
+
+        if(color == black){
+            addLike(this)
+            $(this).css("color", blue)
+        }
+        else{
+            removeLike(this);
+            $(this).css("color", black)
+        }
+
+        function addLike(upvote){
+            var like = $(upvote).siblings('.likes').text();
+            $(upvote).siblings('.likes').text(parseInt(like) + 1);
+        }
+        function removeLike(upvote){
+            var like = $(upvote).siblings('.likes').text();
+            $(upvote).siblings('.likes').text(parseInt(like) - 1);
+        }
+    })
+
+    $(".downvote").click(function(){
+        var color = $(this).css("color");
+        var siblingcolor = $(this).siblings('.upvote').css("color");
+        const black = "rgb(38, 38, 38)";
+        const red = "rgb(255, 0, 0)";
+        
+        if(siblingcolor != black){
+            $(this).siblings('.upvote').css("color", black);
+            removeLike(this);
+        }
+
+        if(color == black){
+            removeLike(this)
+            $(this).css("color", red)
+        }
+        else{
+            addLike(this);
+            $(this).css("color", black)
+        }
+
+        function addLike(downvote){
+            var like = $(downvote).siblings('.likes').text();
+            $(downvote).siblings('.likes').text(parseInt(like) + 1);
+        }
+        function removeLike(downvote){
+            var like = $(downvote).siblings('.likes').text();
+            $(downvote).siblings('.likes').text(parseInt(like) - 1);
+        }
     })
 })
--- public/js/settings.js
+++ public/js/settings.js
@@ -26,30 +26,6 @@ $(document).ready(function () {
     })
 
     $('#save_displayphoto').click(()=>{
-        var fd = new FormData();
-        var files = $('#fileupload')[0].files;
-        
-        // Check file selected or not
-        if(files.length > 0 ){
-           fd.append('file',files[0]);
-
-           $.ajax({
-              url: 'upload.php',
-              type: 'post',
-              data: fd,
-              contentType: false,
-              processData: false,
-              success: function(response){
-                 if(response != 0){
-                    $("#displayphoto").attr("src",response); 
-                    $(".preview img").show(); // Display image element
-                 }else{
-                    alert('file not uploaded');
-                 }
-              },
-           });
-        }else{
-           alert("Please select a file.");
-        }
+       
     })
 })
