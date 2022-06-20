c399450333aa25272bfd0afe75b6ac00861264c5
Kevin Abelgas <61397830+kevvvvin@users.noreply.github.com>
2022-06-20 22:40:00 +0800

    added passport authentication

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,21 +1,31 @@
 import db from '../models/db.js';
 import User from '../models/UserSchema.js';
+import passport from 'passport';
 
 const loginController = {
     getLogin: function(req, res){
         res.render('login');
     },
     
-    loginUser : function(req, res){
-        var user = {
-            username: req.query.username,
-            password: req.query.password
-        }
-        db.findOne(User, user, {}, (result)=>{
-            if(result) res.send(true);
-            else res.send(false);
-        })
-    }
-}
+    loginUser : function(req, res, next){
+        passport.authenticate("local", {
+            successRedirect: "/",
+            failureRedirect: "/login",
+            failureFlash: true
+        })(req, res, next);
+    } 
+    
+};
 
 export default loginController;
+
+/*loginUser : function(req, res){
+    var user = {
+        username: req.body.username,
+        password: req.body.password
+    }
+    db.findOne(User, user, {}, (result)=>{
+        if(result) res.send(true);
+        else res.send(false);
+    })  
+}*/
