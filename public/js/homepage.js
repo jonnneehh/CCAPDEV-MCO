758da51f26caefef0ae63937c8f2c5f1ac15c3c2
Kevin Abelgas <61397830+kevvvvin@users.noreply.github.com>
2022-06-21 04:49:37 +0800

    add post button now working as intended

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -1,22 +1,25 @@
 $(document).ready(function () {
 
-    $(".post-container").click(function (e) { 
+    $(".post-container").on("click", function (e) { 
         e.preventDefault();
-        $(".modal").show();
+        $("#interact_modal").show();
         $(".screen_block").show();
     });
 
-    $(".screen_block").click(function (e) {
+    $(".screen_block").on("click", function (e) {
         e.preventDefault();
         $(".modal").hide();
         $(this).hide();
     });
     
     // removing this will let you click the button which redirects you to /addpost
-    $("#addPostdiv").on("click", function (e) {
-        e.preventDefault();
-        $(".modal").show();
+    $(".addPostdiv").on("click", function (e) {
+        if (!($(".addPostdiv").attr("id"))) {
+            e.preventDefault();
+        }
+        $("#addpost_modal").show();
         $(".screen_block").show()
+        
     });
     
 });
