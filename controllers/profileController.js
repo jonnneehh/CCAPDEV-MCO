9bc3bffe45536f10ce05a72cbce593ea48df366c
Kevin Abelgas <61397830+kevvvvin@users.noreply.github.com>
2022-06-21 22:30:38 +0800

    displays profile with user data

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -5,7 +5,15 @@ import Post from "../models/PostModel.js";
 const profileController = {
     
     getProfile: function (req, res) {
-        res.render("profile");
+
+        // Show user info
+        db.findOne(User, {username: req.user.username}, {}, function (result) {
+            db.findMany(Post, {poster: req.user.username}, {}, function (result2) {
+                result2.reverse();
+                res.render("profile", {userinfo: result, posts: result2})
+            });
+        });
+
     },
 
 }
