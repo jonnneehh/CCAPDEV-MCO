efb739445563e559283e635c4cb9c9e9a5aa475e
Jon Llamado <jon_llamado@dlsu.edu.ph>
2022-07-04 19:42:31 +0800

    profile upvotes and downvotes communication w database should work now

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -5,20 +5,52 @@ import Comment from "../models/CommentsSchema.js";
 
 const profileController = {
     
-    getUserProfile: function (req, res) {
+    getUserProfile: async function (req, res) {
+        var user = await user();
         db.findOne(User, {username: req.params.username}, {}, function (userinfo) {
-            db.findManyToJSON(Post, {poster: req.params.username}, {}, async function (postinfo) {
-                for(let post of postinfo){
+            db.findManyToJSON(Post, {}, null, async function (res_posts) {
+                for(let post of res_posts){
+                    //Add the comments for each post
                     var commentsdata = [];
                     for(let commentid of post.comments){
-                        let c = await addComment(commentid);
+                        //Add the blue and red updoots and downdoots
+                        var votedata = {
+                            isUpvoted: false,
+                            isDownvoted: false
+                        }
+                        if(user){
+                            if(user.upvotedComments.includes(commentid)){
+                                votedata.isUpvoted = true;
+                            }
+                            else if(user.downvotedComments.includes(commentid)){
+                                votedata.isDownvoted = true;
+                            }
+                        }
+    
+                        let c = await addComment(commentid); //Finds commentid
+                        Object.assign(c, votedata); //Appends votedata to c
                         commentsdata.push(c);
                     }
                     post.comments = commentsdata;
-                }
-
-                postinfo.reverse();
-                res.render("profile", {userinfo: userinfo, posts: postinfo});
+    
+                    //Add the blue and red updoots and downdoots
+                    var votedata = {
+                        isUpvoted: false,
+                        isDownvoted: false
+                    }
+                    if(user){
+                        if(user.upvotedPosts.includes(post._id)){
+                            votedata.isUpvoted = true;
+                        }
+                        else if(user.downvotedPosts.includes(post._id)){
+                            votedata.isDownvoted = true;
+                        }
+                    }
+                    Object.assign(post, votedata);
+                }   
+                // Reverses Posts array to show newer posts first
+                res_posts.reverse();
+                res.render("profile", {userinfo: userinfo, posts: res_posts});
             })
         })
 
@@ -27,6 +59,30 @@ const profileController = {
                 db.findOne(Comment, {_id: commentid}, null, function(res_comment){
                     resolve(res_comment) 
                 })
+            })
+        }
+
+        function user(){
+            return new Promise((resolve, reject) =>{
+                try{
+                    var user;
+                    try{
+                        user = req.user.username;
+                    } catch(e){
+                        console.error(e);
+                    }
+
+                    if(!user) {
+                        console.log("No logged in users detected");
+                        resolve(null)
+                    }
+                    db.findOne(User, {username: user}, {}, function(data){
+                        resolve(data)
+                    }) 
+                }  
+                catch(e){
+                    console.error(e);
+                }
             })
         }
     }
